91093c0ef1fa8a1207bbf99259078672ba22b02b
Mouque <henriq8383@gmail.com>
2023-03-29 23:28:47 -0300

    Até four sisters and duckbear

---
+++ Part 1 The Basics/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Escreva a quantidade de ovos de chocolate: (apenas o número)");
+
+int eggsQuantity = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Para quantas irmãs serão divididos os ovos? (digite apenas o número)");
+
+int sisters = Convert.ToInt32(Console.ReadLine());
+
+int remainingEggs = eggsQuantity % sisters;
+int eggsQuantityForEachSister = (eggsQuantity - remainingEggs) / sisters;
+
+if (eggsQuantityForEachSister <= 0)
+{
+    Console.WriteLine("Nenhum ovo será dado para nenhuma irmã.");
+    if (remainingEggs > 0)
+    {
+        Console.WriteLine("Dê os ovos ao Duckbear.");
+    }
+
+}
+
+if (eggsQuantityForEachSister > 0 && remainingEggs == 0)
+{
+    Console.WriteLine("Nenhum ovo será dado ao Duckbear. Assim, cada irmã ficará com " + eggsQuantityForEachSister + " ovos.");
+}
+else if (eggsQuantityForEachSister > 0 && remainingEggs > 0)
+{
+    Console.WriteLine("Serão dados " + remainingEggs + " ovos ao Duckbear. Assim, cada irmã ficará com " + eggsQuantityForEachSister + " ovos.");
+}
+
+
+
---
+++ Part 1 The Basics/TheTriangleFarmer/Program.cs
@@ -0,0 +1,13 @@
+﻿// triangle area formula: area = (base * height) / 2
+
+Console.WriteLine("Vamos calcular o valor da área do triângulo.");
+Console.WriteLine("Insira o valor da base: ");
+
+float triangleBase = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Agora, insira o valor da altura: ");
+
+float triangleHeight = Convert.ToInt32(Console.ReadLine());
+
+float triangleArea = triangleBase * triangleHeight / 2;
+Console.WriteLine("A área do triângulo é: " + triangleArea);
