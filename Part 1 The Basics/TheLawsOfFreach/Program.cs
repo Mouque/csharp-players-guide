8d4bb9660410764bdef5d9952472d87b275ba327
Henrique Moura <henriq8383@gmail.com>
2023-03-31 18:44:32 -0300

    Até Laws of Freach

---
+++ Part 1 The Basics/TheLawsOfFreach/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = new int[5] {5, 5, 5, 5, 5};
+
+int currentSmallest = int.MaxValue;
+foreach (int number in array)
+{
+    if (number < currentSmallest)
+    currentSmallest = number;
+}
+Console.WriteLine($"O menor número da array é {currentSmallest}");
+
+float totalSum = 0f;
+foreach (int number in array)
+{
+    totalSum += number;
+}
+
+float average =  (float)(totalSum / (float)array.Length);
+Console.WriteLine($"A média de todos os números da array é {average}");
---
+++ Part 1 The Basics/TheReplicatorOfD'To/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] userArray = new int[5];
+int[] newArray = new int[] {1, 2, 3, 4, 5};
+
+Console.WriteLine("Insira 5 números abaixo:");
+Console.Write("Primeiro número: ");
+int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+Console.Write("Segundo número: ");
+int segundoNumero = Convert.ToInt32(Console.ReadLine());
+Console.Write("Terceiro número: ");
+int terceiroNumero = Convert.ToInt32(Console.ReadLine());
+Console.Write("Quarto número: ");
+int quartoNumero = Convert.ToInt32(Console.ReadLine());
+Console.Write("Quinto número: ");
+int quintoNumero = Convert.ToInt32(Console.ReadLine());
+
+userArray[0] = primeiroNumero;
+userArray[1] = segundoNumero;
+userArray[2] = terceiroNumero;
+userArray[3] = quartoNumero;
+userArray[4] = quintoNumero;
+
+for (int i = 0; i < userArray.Length; i++)
+{
+    Console.WriteLine($"Antigo número {newArray[i]} mudado para {userArray[i]}, escolhido pelo usuário");
+    newArray[i] = userArray[i];
+    
+}
+
