c7a2509b074628503de30837a3476e08b30f7413
Mouque <henriq8383@gmail.com>
2023-03-31 12:00:42 -0300

    até the magic cannon

---
+++ Part 1 The Basics/TheMagicCannon/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("The Magic Cannon will fire these ammo at each turn: ");
+
+for (int turn = 1; turn <= 100; turn++)
+{
+    if (turn % 3 == 0 && turn % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{turn}: Combined! Electric and Fire!!");
+    }
+    else if (turn % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{turn}: Fire");
+    }
+    else if (turn % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{turn}: Electric");
+
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine($"{turn}: Normal");
+    }
+
+}
---
+++ Part 1 The Basics/ThePrototype/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("User 1, enter a number between 0 and 100: ");
+
+int pickedNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+bool isNumberGuessed = false;
+
+Console.WriteLine("User 2, guess the number.");
+
+while (!isNumberGuessed)
+{
+    Console.Write("What is your guess? ");
+    int guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess > pickedNumber) Console.WriteLine($"{guess} is too high.");
+    else if (guess < pickedNumber) Console.WriteLine($"{guess} is too low");
+    else
+    {
+        Console.WriteLine("You guessed the number!");
+        isNumberGuessed = true;
+    }
+}
