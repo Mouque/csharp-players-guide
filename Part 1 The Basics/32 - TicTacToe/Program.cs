cc8927788b28b1e9d5a1a16a793218b4781ac2fb
Mouque <henriq8383@gmail.com>
2023-04-10 20:24:16 -0300

    Até PackingInventory

---
+++ Part 1 The Basics/32 - TicTacToe/Program.cs
@@ -0,0 +1,216 @@
+﻿TicTacToeGame game = new();
+
+game.StartGame();
+
+class TicTacToeGame
+{
+    RoundHandler roundHandler = new();
+    Table table = new();
+    int round;
+
+    public void StartGame()
+    {
+        Console.WriteLine("Let's play Tic-Tac-Toe!");
+        Console.WriteLine("Remember the square positions below, you will use their numbers to draw the crosses and circles!\n");
+        Console.WriteLine(" 7 | 8 | 9 ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine(" 4 | 5 | 6 ");
+        Console.WriteLine("---+---+---");
+        Console.WriteLine(" 1 | 2 | 3 \n");
+        Console.WriteLine("-------------------------\n");
+        Console.WriteLine("Let's Begin!");
+
+        while (true)
+        {
+            Console.WriteLine($"\nIt's {roundHandler.CurrentTurn} turn!");
+
+            Console.WriteLine($" {table.Position7} | {table.Position8} | {table.Position9} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {table.Position4} | {table.Position5} | {table.Position6} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {table.Position1} | {table.Position2} | {table.Position3} \n");
+
+            if (roundHandler.CurrentTurn == "Cross")
+            {
+                int desiredSquare = RoundHandler.ReceivePlayerDesiredSquare();
+                while (!table.TryDrawCross(desiredSquare))
+                {
+                    Console.WriteLine("Square occupied! Try again!");
+                    desiredSquare = RoundHandler.ReceivePlayerDesiredSquare();
+                }
+            }
+            if (roundHandler.CurrentTurn == "Circle")
+            {
+                int desiredSquare = RoundHandler.ReceivePlayerDesiredSquare();
+                while (!table.TryDrawCircle(desiredSquare))
+                {
+                    Console.WriteLine("Square occupied! Try again!");
+                    desiredSquare = RoundHandler.ReceivePlayerDesiredSquare();
+                }
+            }
+            bool gameState = table.CheckGameEnd();
+            if (gameState)
+            {
+                Console.WriteLine($"\n{table.Winner} won!\n");
+                Console.WriteLine($" {table.Position7} | {table.Position8} | {table.Position9} ");
+                Console.WriteLine("---+---+---");
+                Console.WriteLine($" {table.Position4} | {table.Position5} | {table.Position6} ");
+                Console.WriteLine("---+---+---");
+                Console.WriteLine($" {table.Position1} | {table.Position2} | {table.Position3} \n");
+                break;
+            }
+            roundHandler.ChangeTurn();
+
+        }
+    }
+}
+
+class RoundHandler
+{
+    Turn _turn;
+    public string CurrentTurn
+    {
+        get
+        {
+            return Convert.ToString(_turn);
+        }
+    }
+
+    public RoundHandler()
+    {
+        _turn = Turn.Cross;
+    }
+
+    public void ChangeTurn()
+    {
+        _turn = _turn == Turn.Cross ? Turn.Circle : Turn.Cross;
+    }
+
+    public static int ReceivePlayerDesiredSquare()
+    {
+        Console.Write("What square do you want to play in? ");
+        string input = Console.ReadLine() ?? "10";
+        int square = Convert.ToInt32(input);
+
+        while (square < 1 || square > 9)
+        {
+            Console.Write("Insert a valid square, between 1 and 9: ");
+            input = Console.ReadLine() ?? "10";
+            square = Convert.ToInt32(input);
+        }
+
+        return square;
+    }
+
+    enum Turn { Cross, Circle, }
+}
+
+class Table
+{
+    private bool[] isPositionOccupied = new bool[9];
+    private char[] positions = new char[9] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+
+    string _winner;
+
+    public string Winner
+    {
+        get
+        {
+            return _winner;
+        }
+    }
+
+    public char Position1 { get => positions[0]; }
+    public char Position2 { get => positions[1]; }
+    public char Position3 { get => positions[2]; }
+    public char Position4 { get => positions[3]; }
+    public char Position5 { get => positions[4]; }
+    public char Position6 { get => positions[5]; }
+    public char Position7 { get => positions[6]; }
+    public char Position8 { get => positions[7]; }
+    public char Position9 { get => positions[8]; }
+
+    public bool TryDrawCross(int position)
+    {
+        if (isPositionOccupied[position - 1])
+        {
+            return false;
+        }
+        else
+        {
+            positions[position - 1] = 'X';
+            isPositionOccupied[position - 1] = true;
+            return true;
+        }
+    }
+    public bool TryDrawCircle(int position)
+    {
+        if (isPositionOccupied[position - 1])
+        {
+            return false;
+        }
+        else
+        {
+            positions[position - 1] = 'O';
+            isPositionOccupied[position - 1] = true;
+            return true;
+        }
+    }
+
+
+    public bool CheckGameEnd()
+    {
+        if
+            (
+            (Position7.Equals('X') && Position8.Equals('X') && Position9.Equals('X')) ||
+
+            (Position4.Equals('X') && Position5.Equals('X') && Position6.Equals('X')) ||
+
+            (Position1.Equals('X') && Position2.Equals('X') && Position3.Equals('X')) ||
+
+            (Position7.Equals('X') && Position4.Equals('X') && Position1.Equals('X')) ||
+
+            (Position8.Equals('X') && Position5.Equals('X') && Position2.Equals('X')) ||
+
+            (Position8.Equals('X') && Position5.Equals('X') && Position2.Equals('X')) ||
+
+            (Position9.Equals('X') && Position6.Equals('X') && Position3.Equals('X')) ||
+
+            (Position7.Equals('X') && Position5.Equals('X') && Position3.Equals('X')) ||
+
+            (Position9.Equals('X') && Position5.Equals('X') && Position1.Equals('X'))
+            )
+        {
+            _winner = "Cross";
+            return true;
+        }
+
+        if (
+            (Position7.Equals('O') && Position8.Equals('O') && Position9.Equals('O')) ||
+
+            (Position4.Equals('O') && Position5.Equals('O') && Position6.Equals('O')) ||
+
+            (Position1.Equals('O') && Position2.Equals('O') && Position3.Equals('O')) ||
+
+            (Position7.Equals('O') && Position4.Equals('O') && Position1.Equals('O')) ||
+
+            (Position8.Equals('O') && Position5.Equals('O') && Position2.Equals('O')) ||
+
+            (Position8.Equals('O') && Position5.Equals('O') && Position2.Equals('O')) ||
+
+            (Position9.Equals('O') && Position6.Equals('O') && Position3.Equals('O')) ||
+
+            (Position7.Equals('O') && Position5.Equals('O') && Position3.Equals('O')) ||
+
+            (Position9.Equals('O') && Position5.Equals('O') && Position1.Equals('O'))
+            )
+        {
+            _winner = "Circle";
+            return true;
+        }
+
+        return false;
+
+    }
+}
+
---
+++ Part 1 The Basics/33 - PackingInventory/Program.cs
@@ -0,0 +1,142 @@
+﻿Console.WriteLine("Welcome.");
+
+Pack pack = new(5, 8f, 5f);
+
+Console.WriteLine("Pack created.");
+while (true)
+{
+    Console.WriteLine("-------------------------------------");
+    Console.WriteLine(
+    $"Items Slots: {pack.CurrentItemSlots}/{pack.ItemSlotsLimit} | " +
+    $"Weight Capacity: {pack.CurrentWeight}/{pack.WeightLimit} | " +
+    $"Volume Capacity: {pack.CurrentVolume}/{pack.VolumeLimit}"
+                 );
+    Console.WriteLine("Which item do you want to add?");
+    Console.WriteLine("1: Arrow (Weight: 0.1 Volume: 0.05");
+    Console.WriteLine("2: Bow (Weight: 1 Volume: 4");
+    Console.WriteLine("3: Rope (Weight: 1 Volume: 1.5");
+    Console.WriteLine("4: Water (Weight: 2 Volume: 3");
+    Console.WriteLine("5: Food (Weight: 1 Volume: 0.5");
+    Console.WriteLine("6: Sword (Weight: 5 Volume: 3");
+    Console.Write("Number: ");
+    string input = Console.ReadLine();
+    while (input == null)
+    {
+        Console.Write("Not a number, try again: ");
+        input = Console.ReadLine();
+    }
+    switch (Convert.ToInt32(input))
+    {
+        default:
+            Console.WriteLine("Not a valid option");
+            break;
+        case 1:
+            Arrow arrow = new();
+            string attempt = pack.Add(arrow) ? "Arrow added" : "Arrow couldn't be added";
+            Console.WriteLine(attempt);
+            break;
+        case 2:
+            Bow bow = new();
+            attempt = pack.Add(bow) ? "Bow added" : "Bow couldn't be added";
+            Console.WriteLine(attempt);
+            break;
+        case 3:
+            Rope rope = new();
+            attempt = pack.Add(rope) ? "Rope added" : "Rope couldn't be added";
+            Console.WriteLine(attempt);
+            break;
+        case 4:
+            Water water = new();
+            attempt = pack.Add(water) ? "Water added" : "Water couldn't be added";
+            Console.WriteLine(attempt);
+            break;
+        case 5:
+            Food food = new();
+            attempt = pack.Add(food) ? "Food added" : "Food couldn't be added";
+            Console.WriteLine(attempt);
+            break;
+        case 6:
+            Sword sword = new();
+            attempt = pack.Add(sword) ? "Sword added" : "Sword couldn't be added";
+            Console.WriteLine(attempt);
+            break;
+    }
+}
+
+
+
+
+
+
+class Pack
+{
+    public int ItemSlotsLimit { get; private set; }
+    public float WeightLimit { get; private set; }
+    public float VolumeLimit { get; private set; }
+    public int CurrentItemSlots { get; private set; }
+    public float CurrentWeight { get; private set; }
+    public float CurrentVolume { get; private set; }
+
+    public InventoryItem[] ItemList { get; private set; }
+
+    public Pack(int itemSlotsLimit, float weightLimit, float volumeLimit)
+    {
+        ItemSlotsLimit = itemSlotsLimit;
+        WeightLimit = weightLimit;
+        VolumeLimit = volumeLimit;
+        ItemList = new InventoryItem[itemSlotsLimit];
+    }
+
+    public bool Add(InventoryItem item)
+    {
+        if (CurrentWeight + item.Weight > WeightLimit)
+        {
+            Console.WriteLine("Too much weight");
+            return false;
+        }
+
+        if (CurrentVolume + item.Volume > VolumeLimit)
+        {
+            Console.WriteLine("Too much volume");
+            return false;
+        }
+        if (CurrentItemSlots == ItemSlotsLimit)
+        {
+            Console.WriteLine("Pack is full");
+            return false;
+        }
+
+        CurrentWeight += item.Weight;
+        CurrentVolume += item.Volume;
+        for (int i = 0; i < ItemList.Length; i++)
+        {
+            if (ItemList[i] == null)
+            {
+                ItemList[i] = item;
+                CurrentItemSlots++;
+                break;
+            }
+            else continue;
+        }
+        return true;
+    }
+}
+
+class Sword : InventoryItem { public Sword() : base(5f, 3f) { } }
+class Food : InventoryItem { public Food() : base(1f, 0.5f) { } }
+class Water : InventoryItem { public Water() : base(2f, 3f) { } }
+class Rope : InventoryItem { public Rope() : base(1f, 1.5f) { } }
+class Bow : InventoryItem { public Bow() : base(1f, 4f) { } }
+class Arrow : InventoryItem { public Arrow() : base(0.1f, 0.05f) { } }
+class InventoryItem
+{
+
+    public float Weight { get; private set; }
+    public float Volume { get; private set; }
+
+    public InventoryItem(float weight, float volume)
+    {
+        Weight = weight;
+        Volume = volume;
+    }
+}
