41883360435591451086033b05bce589f6ad8d19
Henrique Moura <henriq8383@gmail.com>
2023-04-27 09:02:20 -0300

    War Preparations

---
+++ Part 2 Object-Oriented Programming/38 - WarPreparations/Program.cs
@@ -0,0 +1,33 @@
+﻿MainMethod();
+
+void MainMethod()
+{
+    Sword basicSword = new(SwordMaterials.Iron, GemstoneTypes.None, 1, 3);
+    Sword eliteSword = basicSword with { GemstoneType = GemstoneTypes.Sapphire };
+    Sword mythicalSword = basicSword with { SwordMaterial = SwordMaterials.Binarium, GemstoneType = GemstoneTypes.Bitstone };
+
+    Console.WriteLine(basicSword);
+    Console.WriteLine(eliteSword);
+    Console.WriteLine(mythicalSword);
+}
+
+public record Sword(SwordMaterials SwordMaterial, GemstoneTypes GemstoneType, int Length, int CrossguardWidth);
+
+public enum SwordMaterials
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium,
+}
+
+public enum GemstoneTypes
+{
+    None,
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone,
+}
