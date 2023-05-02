87ce49c082c4d1bcd8c4fa2ef646a99de87e523a
Henrique Moura <henriq8383@gmail.com>
2023-05-02 18:07:20 -0300

    Starting The Fountain of Objects project

---
+++ Part 2 Object-Oriented Programming/40 - TheFountainOfObjects/Program.cs
@@ -0,0 +1,32 @@
+﻿public class CavernGameTable
+{
+    private RoomContents[,] tableRoomPositions;
+
+    public CavernGameTable()
+    {
+        tableRoomPositions = new RoomContents[4, 4];
+        for (int row = 0; row < tableRoomPositions.GetLength(0); row++)
+        {
+            for (int column = 0; column < tableRoomPositions.GetLength(1); column++)
+            {
+                tableRoomPositions[row, column] = RoomContents.Empty;
+            }
+        }
+
+        tableRoomPositions[0, 0] = RoomContents.Entrance;
+        tableRoomPositions[0, 2] = RoomContents.Fountain;
+    }
+
+    public void DisplayTable()
+    {
+
+    }
+
+    enum RoomContents 
+    { 
+        Empty,
+        Entrance,
+        Fountain,
+    }
+
+}
