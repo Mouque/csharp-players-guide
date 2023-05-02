f0f329dddb9f6d36208e034f85ff7d0ed8e28e01
Henrique Moura <henriq8383@gmail.com>
2023-05-02 16:44:17 -0300

    Colored Items

---
+++ Part 2 Object-Oriented Programming/39 - ColoredItems/Program.cs
@@ -0,0 +1,34 @@
+﻿MainMethod();
+
+void MainMethod()
+{
+    ColoredItem<Sword> sword = new(new Sword(), ConsoleColor.Blue);
+    sword.Display();
+    ColoredItem<Bow> bow = new(new Bow(), ConsoleColor.Red);
+    bow.Display();
+    ColoredItem<Axe> axe = new(new Axe(), ConsoleColor.Green);
+    axe.Display();
+}
+
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T>
+{
+    public T Item { get; }
+    public ConsoleColor Color { get; }
+
+    public ColoredItem(T item, ConsoleColor color)
+    {
+        Item = item;
+        Color = color;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item.ToString());
+    }
+}
