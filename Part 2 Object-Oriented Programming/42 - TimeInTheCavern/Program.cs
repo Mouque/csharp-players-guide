07e938959f703a18bc184ddeb00b8d38d6c69039
Henrique Moura <henriq8383@gmail.com>
2023-05-05 19:01:28 -0300

    até List of Commands

---
+++ Part 2 Object-Oriented Programming/42 - TimeInTheCavern/Program.cs
@@ -0,0 +1,298 @@
+﻿
+CavernGame game = new();
+game.RunGame();
+
+
+public class CavernGame
+{
+    CavernGameTable gameTable = new();
+    Player player = new();
+
+    public void RunGame()
+    {
+        TimeHandler timeCounter = new();
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("You have entered on the cavern of the Fountain of Objects!\nFind the fountain, " +
+            "turn it on and return to the entrance! " +
+            "You can type \"Help\" to see the list of commands.");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        while (true)
+        {
+            Console.WriteLine("-------------------------------------------------------");
+
+            if (IsGameWin(player, gameTable))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("You have turned on the fountain and returned to the entrance! You won!");
+                Console.WriteLine($"You took {timeCounter.GetConclusionTime().Minutes} minutes and {timeCounter.GetConclusionTime().Seconds} seconds to conclude the game!");
+                Console.ForegroundColor = ConsoleColor.White;
+                break;
+            }
+
+            TextHandler.DisplayAndDescribePlayerLocation(player, gameTable);
+            GameInput.AskForCommand(player, gameTable);
+
+
+        }
+    }
+
+    private bool IsGameWin(Player player, CavernGameTable gameTable)
+    {
+        if (gameTable.GetEntrancePosition() == (player.Row, player.Column) && gameTable.GetIsFountainOn())
+        {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class TimeHandler
+{
+    private DateTime startTime = DateTime.Now;
+    private TimeSpan conclusionTime;
+
+
+    public TimeSpan GetConclusionTime()
+    {
+        conclusionTime = DateTime.Now - startTime;
+
+        return conclusionTime;
+    }
+}
+
+public static class TextHandler
+{
+
+
+    public static void DisplayAndDescribePlayerLocation(Player player, CavernGameTable gameTable)
+    {
+        Console.WriteLine($"You are in the room at (Row = {player.Row}, Column = {player.Column})");
+
+
+
+        if (gameTable.GetEntrancePosition() == (player.Row, player.Column))
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("You see light in this room coming from outside the cavern. This is the entrance.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        if (gameTable.GetFountainPosition() == (player.Row, player.Column))
+        {
+            if (!gameTable.GetIsFountainOn())
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("You hear water dripping in this room. The Fountain of Objects is here!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("You hear the rushing waters from the Fountain of Objects. It has been reactivated!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+
+
+    }
+}
+
+public class GameInput
+{
+    public static void AskForCommand(Player player, CavernGameTable gameTable)
+    {
+        Console.Write("What do you want to do? ");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        string input = Console.ReadLine() ?? "null";
+        Console.ForegroundColor = ConsoleColor.White;
+
+        Commands command = input.ToLower() switch
+        {
+            "move north" => new MoveNorth(),
+            "move south" => new MoveSouth(),
+            "move east" => new MoveEast(),
+            "move west" => new MoveWest(),
+            "turn on fountain" => new TurnOnFountain(),
+            "help" => new Help(),
+            "null" => new InvalidCommand(),
+            _ => new InvalidCommand(),
+        };
+
+        command.Run(player, gameTable);
+    }
+
+}
+
+public interface Commands
+{
+    public void Run(Player player, CavernGameTable gameTable);
+
+}
+
+public class MoveNorth : Commands
+{
+    public void Run(Player player, CavernGameTable gameTable) => player.MoveNorth();
+}
+
+public class MoveSouth : Commands
+{
+    public void Run(Player player, CavernGameTable gameTable) => player.MoveSouth();
+}
+
+public class MoveEast : Commands
+{
+    public void Run(Player player, CavernGameTable gameTable) => player.MoveEast();
+}
+
+public class MoveWest : Commands
+{
+    public void Run(Player player, CavernGameTable gameTable) => player.MoveWest();
+}
+
+public class TurnOnFountain : Commands
+{
+
+    public void Run(Player player, CavernGameTable gameTable)
+    {
+        if (gameTable.GetFountainPosition() == (player.Row, player.Column))
+        {
+            gameTable.TurnOnFountain();
+        }
+    }
+}
+
+public class Help : Commands
+{
+    public void Run(Player player, CavernGameTable gameTable)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("Here is the list of possible commands:");
+        Console.WriteLine("Move North");
+        Console.WriteLine("Move South");
+        Console.WriteLine("Move East");
+        Console.WriteLine("Move West");
+        Console.WriteLine("Turn on Fountain");
+        Console.WriteLine("Help");
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+public class InvalidCommand : Commands
+{
+    public void Run(Player player, CavernGameTable gameTable) => Console.WriteLine("Invalid command");
+}
+
+public class Player
+{
+    public int Row { get; private set; } = 0;
+    public int Column { get; private set; } = 0;
+
+    public void MoveNorth() { if (Row > 0) Row--; }
+
+    public void MoveSouth() { if (Row < 3) Row++; }
+
+    public void MoveEast() { if (Column < 3) Column++; }
+
+    public void MoveWest() { if (Column > 0) Column--; }
+
+
+}
+
+public class CavernGameTable
+{
+    private RoomContents[,] tableRoomPositions;
+    private bool isFountainOn = false;
+
+    public CavernGameTable()
+    {
+        tableRoomPositions = new RoomContents[4, 4];
+        for (int row = 0; row < tableRoomPositions.GetLength(0); row++)
+        {
+            for (int column = 0; column < tableRoomPositions.GetLength(1); column++)
+            {
+                tableRoomPositions[row, column] = RoomContents.Empty;
+            }
+        }
+
+        tableRoomPositions[0, 0] = RoomContents.Entrance;
+        tableRoomPositions[3, 2] = RoomContents.Fountain;
+    }
+
+    public (int, int) GetEntrancePosition()
+    {
+        int entranceRowPosition = 0;
+        int entranceColumnPosition = 0;
+
+        for (int row = 0; row < tableRoomPositions.GetLength(0); row++)
+        {
+            for (int column = 0; column < tableRoomPositions.GetLength(1); column++)
+            {
+                if (tableRoomPositions[row, column] == RoomContents.Entrance)
+                {
+                    entranceRowPosition = row;
+                    entranceColumnPosition = column;
+                }
+            }
+        }
+
+        return (entranceRowPosition, entranceColumnPosition);
+    }
+
+    public (int, int) GetFountainPosition()
+    {
+        int fountainRowPosition = 0;
+        int fountainColumnPosition = 0;
+
+        for (int row = 0; row < tableRoomPositions.GetLength(0); row++)
+        {
+            for (int column = 0; column < tableRoomPositions.GetLength(1); column++)
+            {
+                if (tableRoomPositions[row, column] == RoomContents.Fountain)
+                {
+                    fountainRowPosition = row;
+                    fountainColumnPosition = column;
+                }
+            }
+        }
+
+        return (fountainRowPosition, fountainColumnPosition);
+    }
+
+    public bool GetIsFountainOn() => isFountainOn;
+
+    public void TurnOnFountain() => isFountainOn = true;
+
+    public void DisplayTableWithPlayer(Player player)
+    {
+        for (int row = 0; row < 4; row++)
+        {
+            Console.Write("| ");
+            for (int column = 0; column < 4; column++)
+            {
+                if (player.Row == row && player.Column == column)
+                {
+                    Console.Write(tableRoomPositions[row, column] + " with Player ");
+                    Console.Write(" | ");
+                }
+                else
+                {
+                    Console.Write(tableRoomPositions[row, column]);
+                    Console.Write(" | ");
+                }
+            }
+            Console.WriteLine("");
+        }
+
+
+    }
+
+    enum RoomContents
+    {
+        Empty,
+        Entrance,
+        Fountain,
+    }
+
+}
---
+++ Part 2 Object-Oriented Programming/43 - ListOfCommands/Program.cs
@@ -0,0 +1,128 @@
+﻿MainMethod();
+
+string AskForCommand()
+{
+    string command = Console.ReadLine();
+    while (command == null)
+    {
+        Console.Write("Invalid command, try again: ");
+        command = Console.ReadLine();
+    }
+    return command;
+}
+
+void MainMethod()
+{
+    Console.WriteLine("Valid commands: on, off, north, south, west, east, stop.");
+
+    Robot robot = new Robot();
+
+    while (true)
+    { 
+        Console.Write("Command: ");
+
+        string input = AskForCommand();
+
+        if (input == "stop") break;
+
+        IRobotCommand newCommand = input.ToLower() switch
+        {
+            "on" => new OnCommand(),
+            "off" => new OffCommand(),
+            "north" => new NorthCommand(),
+            "south" => new SouthCommand(),
+            "west" => new WestCommand(),
+            "east" => new EastCommand(),
+            _ => new InvalidCommand(input),
+        };
+        robot.Commands.Add(newCommand);
+    }
+
+    robot.Run();
+    
+}
+
+public interface IRobotCommand
+{
+    public void Run(Robot robot);
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y++;
+    }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.Y--;
+    }
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X--;
+    }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered) robot.X++;
+    }
+}
+
+public class InvalidCommand : IRobotCommand
+{
+    private string commandToDisplay;
+
+    public InvalidCommand(string command)
+    {
+        commandToDisplay = command;
+    }
+
+    public void Run(Robot robot)
+    {
+        Console.WriteLine($"Invalid command \"{commandToDisplay}\"");
+    }
+}
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public List<IRobotCommand> Commands = new List<IRobotCommand>();
+    public void Run()
+    {
+        foreach (IRobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            if (command is InvalidCommand) continue;
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
